374fae4a4c7fb3933b8f32dc6abfe7f1789d4832
RahulNathani49 <nathanirahul49@gmail.com>
2019-04-24 17:19:15 +0530

    cart

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var sumnew=0;
+var dropitemscount=0;
 function dragStart(event) {
     event.dataTransfer.setData("img", event.target.id);
 
@@ -7,28 +9,77 @@ function allowDrop(event) {
     event.preventDefault();
 }
 
-function drop(event) {
+var display = document.getElementById("display");
+   display.setAttribute("id","rate");
+
+function drop(event,x) {
+    
+    var cartitem = document.getElementById("cartcount");
+    cartitem.innerHTML=dropitemscount;
+    
+
     event.preventDefault();
     var data = event.dataTransfer.getData("img");
     event.target.appendChild(document.getElementById(data));
+
+    var t = event.target.childNodes;
+    console.log(t);
+    var length = event.target.childNodes.length;
+    
+    
+    
+    
+    
+    var i;
+    var sum=0;
+    var j;
+    var count = [];
+   for(j=0,i=1;i<length;i++,j++){
+       
+        count[j]=t[i].childNodes[3].innerHTML;
+    count[j]=parseInt(count[j]);
+       sum=sum+count[j];
+       
+       
+       console.log(count[j]);
+    t[i].childNodes[5].style.display="block";
+       
+   }
+    
+   display.innerHTML="TOTAL = " + sum + "rs only";
+    document.getElementById("cartcount").innerHTML=length-1;
 }
 
-function count() {
-    var display = document.getElementById("display");
-    var dropped = document.getElementById("drop");
-    console.log(dropped.childNodes);
-    var i;
-    var count = [];
-    for (i = 0; i < 6; i++) {
-        count[i] = document.getElementsByClassName("price")[i].innerHTML;
-        count[i] = parseInt(count[i]);
-        console.log(count[i]);
-    }
-    sum=0
-    for(i=0;i<6;i++){
-        sum=sum+count[i];
-    }
-   display.innerHTML=sum + "rs only";
-  
+
+function delitem(x){
     
+    var box = x.parentNode;
+    var i=0;
+   var main = document.getElementById("main");
+   var len=  main.childNodes[1].childNodes.length;
+
+    
+    for(i=1;i<len;i++){
+        if(main.childNodes[1].childNodes[i].childNodes[1].childNodes.length==2){
+        main.childNodes[1].childNodes[i].childNodes[1].appendChild(box);
+        box.childNodes[5].style.display="none";
+            
+        }
+        i++;
+    }
+    var ns=0;
+   var nod = document.getElementById("droppeditem").childNodes.length;
+    var newsum=[];
+    for(i=1;i<nod;i++){
+    newsum[i]=document.getElementById("droppeditem").childNodes[i].childNodes[3].innerHTML;
+        newsum[i]=parseInt(newsum[i]);
+        ns=ns+newsum[i];
+    }
+    display.innerHTML="TOTAL = " + ns + "rs only";
+   var newcartcount = document.getElementById("cartcount").innerHTML; 
+    newcartcount=newcartcount-1;
+   document.getElementById("cartcount").innerHTML=newcartcount;
 }
+    
+    
+
